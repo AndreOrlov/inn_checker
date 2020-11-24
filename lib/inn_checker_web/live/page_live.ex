03b35b76eddc4f4290c5fe977bfa31bed9c570ea
Andre Orlov <to@andreorlov.ru>
2020-11-25 02:19:30 +0300

    Add remote_ip, list of history requests

--- lib/inn_checker_web/live/page_live.ex
+++ lib/inn_checker_web/live/page_live.ex
@@ -1,16 +1,33 @@
 defmodule InnCheckerWeb.PageLive do
+  @moduledoc false
+
   use InnCheckerWeb, :live_view
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
+  def mount(_params, %{"remote_ip" => remote_ip} = _session, socket) do
+    # TODO: rad
+    IO.inspect({SESSION, _session})
+
+    # TODO: удалить ненужные аттрибуты
+    {:ok, assign(socket, query: "", results: %{}, inn_value: "", history_queries: [], remote_ip: remote_ip)}
   end
 
+  # TODO: rad
   @impl true
   def handle_event("suggest", %{"q" => query}, socket) do
     {:noreply, assign(socket, results: search(query), query: query)}
   end
 
+  @impl true
+  def handle_event("inn_check", %{"inn-value" => inn_value} = params, socket) do
+    # TODO: rad
+    IO.inspect({inn_value, socket |> Map.to_list(), socket.assigns[:history_queries], params})
+
+    history_queries = [inn_value | socket.assigns[:history_queries]]
+    {:noreply, assign(socket, inn_value: inn_value, history_queries: history_queries)}
+  end
+
+  # TODO: rad
   @impl true
   def handle_event("search", %{"q" => query}, socket) do
     case search(query) do
@@ -25,6 +42,7 @@ defmodule InnCheckerWeb.PageLive do
     end
   end
 
+  # TODO: rad
   defp search(query) do
     if not InnCheckerWeb.Endpoint.config(:code_reloader) do
       raise "action disabled when not in development"
@@ -35,5 +53,10 @@ defmodule InnCheckerWeb.PageLive do
         String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
         into: %{},
         do: {app, vsn}
+  end
+
+  defp tuple_to_str(tuple, connector_str \\ ".")
+  defp tuple_to_str(tuple, connector) when is_tuple(tuple) do
+    tuple |> Tuple.to_list() |> Enum.join(connector)
   end
 end
