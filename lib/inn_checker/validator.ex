827af8d6f19000057ad228d0c8b551954ef35cb8
Andre Orlov <to@andreorlov.ru>
2020-11-25 20:19:50 +0300

    Working validator for 10 digits inn

--- lib/inn_checker/validator.ex
+++ lib/inn_checker/validator.ex
@@ -1,35 +1,34 @@
 defmodule InnChecker.Validator do
   @moduledoc false
 
-
-  def is_valid(multipliers, inn_string) when String.is_length(inn_string) == 10 do
+  def is_valid(multipliers, inn_string) do
     with {:ok, digit_list} <- convert_to_digit_list(inn_string),
       multiplied = multiply(digit_list),
-      ten_digit = Enum.at(9),
-      ^ten_digit <- calc_magic_number(multiplied)
+      tenth_digit = Enum.at(digit_list, 9),
+      ^tenth_digit <- calc_magic_number(multiplied)
     do
       true
     else
-      false
+      _ -> false
     end
   end
 
   # private
 
-  # TODO: конвертировать в цифры!
-  def convert_to_digit_list(str) do
+  defp convert_to_digit_list(str) do
     case Integer.parse(str) do
-      {_, ""} -> {:ok, String.to_char(str)}
-      _       -> {:error, :string_is_not_number}
+      {number, ""} -> {:ok, Integer.digits(number)}
+      _            -> {:error, :string_is_not_number}
     end
   end
 
-  def multiply(multipliers, inn_digit_list) do
-    Enum.zip(multipliers, inn_string) |> Enum.reduce(0, fn {i1, i2}, acc -> i1 * i2 + acc end)
+  @multipliers [2, 4, 10, 3, 5, 9, 4, 6, 8]
+  defp multiply(inn_digit_list) do
+    Enum.zip(@multipliers, inn_digit_list) |> Enum.reduce(0, fn {i1, i2}, acc -> i1 * i2 + acc end)
   end
 
   @magic_digit 11
-  def calc_magic_number(multiplied) do
+  defp calc_magic_number(multiplied) do
     res = multiplied - div(multiplied, @magic_digit) * @magic_digit
     case res do
       10 -> 0
