1ad758ddcb1a712a97be661fc45cabd4488e592b
Andre Orlov <to@andreorlov.ru>
2020-11-28 11:12:28 +0300

    Remove debug code

--- lib/inn_checker_web/live/login_live.ex
+++ lib/inn_checker_web/live/login_live.ex
@@ -3,11 +3,45 @@ defmodule InnCheckerWeb.LoginLive do
 
   use InnCheckerWeb, :live_view
 
-  @impl true
-  def mount(_params, _session, socket) do
-    # TODO: rad
-    # if connected?(socket), do: send(self(), :update)
+  alias InnChecker.Schema.User
+  alias InnChecker.Session
 
-    {:ok, socket}
+  @impl true
+  def mount(_params, %{"session_id" => session_id} = session, socket) do
+    assigns = Map.put(default_assigns(), :session_id, session_id)
+
+    {:ok, assign(socket, assigns)}
+  end
+
+  @impl true
+  def handle_event("login", %{"login" => login, "password" => password} = _params, %{assigns: %{session_id: session_id}} = socket) do
+    {user, socket} =
+      case verify_user(login, password) do
+        {:ok, user} -> {user, put_flash(socket, :info, "You are logged")}
+        _           -> {nil, put_flash(socket, :error, "Wrong login or password")}
+      end
+    Session.put(session_id, :user, user)
+
+    {:noreply, push_redirect(socket, to: "/admin", replace: true)}
+  end
+  def handle_event("login", _params, socket) do
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info({:user_logged_in, user}, %{assigns: %{session_id: session_id}} = socket) do
+    Session.put(session_id, :user, user)
+    {:noreply, assign(socket, :user, user)}
+  end
+
+  # private
+
+  defp default_assigns(), do: %{login: "", password: ""}
+
+  defp verify_user(login, password) do
+    case User.verify_user(%{login: login, password: password}) do
+      {:ok, user} = res -> res
+      _                 -> {:error, :invalid_user}
+    end
   end
 end
